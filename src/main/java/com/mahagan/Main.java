fafe3293b1f4a07ea453b457107dcb8c19a9df8b
Abhishek Kanawade <tech.abhishk@gmail.com>
2023-08-21 12:06:33 +0530

    Add space

--- src/main/java/com/mahagan/Main.java
+++ src/main/java/com/mahagan/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext applicationContext=new ClassPathXmlApplicationContext("application-properties.xml");
         ProjectDao projectDao=applicationContext.getBean("projectDao", ProjectDao.class);
+
         int IntroCount=projectDao.UpdateProjects(1,"Updated Record");
         System.out.println("record updated"+IntroCount);
 
